51a68dd105cca26eba2303f9875e781873f0c2fd
liufq <popsay@163.com>
2025-01-14 17:35:54 +0800

    1.遥控器增加速度范围

--- bxi_hw_release/share/visualization/package.sh
+++ bxi_hw_release/share/visualization/package.sh
@@ -79,6 +79,7 @@ _colcon_package_sh_source_script() {
 
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/visualization/hook/cmake_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/visualization/hook/ld_library_path_lib.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/visualization/local_setup.sh"
 
 unset _colcon_package_sh_source_script
